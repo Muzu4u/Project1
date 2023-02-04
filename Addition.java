190aa1965153e0fbbeabc3ea6c5aab111d8eda84
Muzammil <muzu4oman@gmail.com>
2023-02-04 10:19:42 +0000

    updating code

--- Addition.java
+++ Addition.java
@@ -1,25 +1,14 @@
-import java.util.Scanner;
-import javax.swing.JOptionPane;
-
-public class Addition 
-{
-
-    public static void main(String[] args) 
-    {
-        int num1;
-        int num2;
-        String operation;
-
-
-//        Scanner input = new Scanner(System.in);
-
-        System.out.println("please enter the first number");
-        num1 = env.num1;
-	System.out.println(env.num1);
-        System.out.println("please enter the second number");
-        num2 = env.num2;
-
-            System.out.println("your answer is: " + (env.num1 + env.num2));
-
-    }
-}
+import java.util.*;  
+class Addition   
+{  
+public static void main(String[] args)  
+{  
+// Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
+System.out.print("Enter first number- ");  
+int a= $num1;  
+System.out.print("Enter second number- ");  
+int b= $num2;    
+int c=a+b;  
+System.out.println("Total= " +c);  
+}  
+}  
