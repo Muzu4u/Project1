c0575498d2fe95b6ff5f43df64e96b2e7bff5c23
Muzammil <muzu4oman@gmail.com>
2023-02-04 11:00:12 +0000

    code is updated

--- Addition.java
+++ Addition.java
@@ -5,9 +5,9 @@ public static void main(String[] args)
 {  
 // Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
 System.out.print("Enter first number- ");  
-int a= $num1;  
+int a= ${num1};  
 System.out.print("Enter second number- ");  
-int b= $num2;    
+int b= ${num2};    
 int c=a+b;  
 System.out.println("Total= " +c);  
 }  
