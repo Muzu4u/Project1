660435600de58709c14334912e7738ef13102c40
Muzammil <muzu4oman@gmail.com>
2023-03-06 11:58:48 +0000

    comtting stretford

--- stretford.java
+++ stretford.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is Stretford"); 
+        System.out.println("This is Stretford");
+	System.out.println("We are in Stretford");
     }
 }
